e263088a26966b9d549c4b0c5c38a2cdc2104689
Felipe Benevides <felipebenevides@outlook.com>
2018-12-23 17:11:25 -0200

    Montagem da Arquitetura Padrão DDD

---
+++ SWAPI/SWAPI.Domain/Models/People.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SWAPI.Domain.Models
+{
+    [DataContract]
+    public class People
+    {
+        public List<People> ListaPessoas { get; set; }
+
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        string birth_year { get; set; }
+        [DataMember]
+        string eye_color { get; set; }
+        [DataMember]
+        string gender { get; set; }
+        [DataMember]
+        string hair_color { get; set; }
+        [DataMember]
+        string height { get; set; }
+        [DataMember]
+        string mass { get; set; }
+        [DataMember]
+        string skin_color { get; set; }
+        [DataMember]
+        string homeworld { get; set; }
+        [DataMember]
+        string created { get; set; }
+        [DataMember]
+        string edited { get; set; }
+        [DataMember]
+        string url { get; set; }
+
+        [DataMember]
+        public string[] Films { get; set; }
+
+        [DataMember]
+        string[] species { get; set; }
+        [DataMember]
+        string[] starships { get; set; }
+        [DataMember]
+        string[] vehicles { get; set; }
+    }
+
+    [DataContract]
+    public class Json
+    {
+        [DataMember]
+        public List<People> results { get; set; }
+    }
+
+
+}
---
+++ SWAPI/SWAPI/Controllers/SWAPIController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SWAPI.Domain.Interfaces.Service;
+
+namespace SWAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class SWAPIController : Controller
+    {
+        private readonly IPeopleService _peopleService;
+
+        public SWAPIController(IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
+        //public async Task<List<People>> ObterPersonagens()
+        //{
+        //    return _peopleService.ObterPersonagens();
+        //}
+
+
+    }
+}
