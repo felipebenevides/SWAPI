508465ab7b67b2c58f2d56e63fc94a94f2e30af8
Felipe Benevides <felipebenevides@outlook.com>
2018-12-23 22:17:28 -0200

    Inclusão da Interface Repository na camada Domain

---
+++ SWAPI/SWAPI.Domain/Interfaces/Interface/IPeopleRepository.cs
@@ -0,0 +1,11 @@
+﻿using SWAPI.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWAPI.Domain.Interfaces.Interface
+{
+    public interface IPeopleRepository : IRepository<People>
+    {
+    }
+}
---
+++ SWAPI/SWAPI.Domain/Interfaces/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWAPI.Domain.Interfaces.Interface
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        void Incluir(TEntity entity);
+
+        void Excluir(Object id);
+
+        void Alterar(TEntity entity);
+
+        IEnumerable<TEntity> Listar();
+
+        TEntity Obter(Object id);
+    }
+}
